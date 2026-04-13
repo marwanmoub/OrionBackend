81c78cfd240f5d058805a37e8660f013eb188672
marwan <moubayedmarwancoding@gmail.com>
2026-04-13 22:48:31 +0300

    create signup

--- controllers/auth.controllers.js
+++ controllers/auth.controllers.js
@@ -1,5 +1,5 @@
-import { generateAccessToken, generateRefreshToken } from "../utils/token";
-import prisma from "/lib/prisma.ts";
+import { generateAccessToken, generateRefreshToken } from "../utils/token.js";
+import prisma from "../lib/prisma.js";
 import argon2 from "argon2";
 
 const authController = {
@@ -9,7 +9,7 @@ const authController = {
 
       let user;
       if (email) {
-        user = prisma.users.findUnique({
+        user = prisma.user.findUnique({
           where: {
             email: email,
           },
@@ -17,7 +17,7 @@ const authController = {
       }
 
       if (phone) {
-        user = prisma.users.findUnique({
+        user = prisma.user.findUnique({
           where: {
             phone: phone,
           },
@@ -54,6 +54,7 @@ const authController = {
   },
 
   signUp: async (req, res) => {
+    console.log("here");
     try {
       let { fullName, email, phone, password } = req.body;
       const hashedPassword = await argon2.hash(password);
@@ -76,11 +77,13 @@ const authController = {
       });
 
       if (!createUser) {
+        console.log("failed");
         return res.status(400).json({
           message: "Failed to create user",
         });
       }
 
+      console.log(createUser);
       return res.status(201).json({
         message: "User created successfully",
         user: {
--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 import express from "express";
-import authRouter from "./routes/auth.routes";
+import authRouter from "./routes/auth.routes.js";
 
 const app = express();
-const PORT = 3000;
+const PORT = 3005;
 
 app.use(express.json());
 
 //ROUTES
 app.use("/auth", authRouter);
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
