14e775aeb147c724869fe9a2ee006c096efbe912
marwan <moubayedmarwancoding@gmail.com>
2026-04-13 20:28:23 +0300

    add sign up

--- controllers/auth.controllers.js
+++ controllers/auth.controllers.js
@@ -1,4 +1,5 @@
 import prisma from "/lib/prisma.ts";
+import argon2 from "argon2";
 
 const authController = {
   login: async (req, res) => {
@@ -38,26 +39,40 @@ const authController = {
 
   signUp: async (req, res) => {
     try {
-      let { email, password } = req.body;
-
-      let userFromDB = prisma.users.findUnique({
-        where: {
-          email: email,
-        },
-      }); // TODO: Fix table name
-      if (userFromDB) {
-        return res.status(400).send("Email already exists");
-      }
+      let { fullName, email, phone, password } = req.body;
       const hashedPassword = await argon2.hash(password);
-      const isValid = await argon2.verify(userFromDB.password, password);
-      if (!isValid) return res.status(401).json({ error: "Wrong Password" });
-      const token = jwt.sign({ userId: user.id }, "Allah_y5lilna_ye@bau$$", {
-        expiresIn: "1h",
+
+      const userProperties = {
+        fullName,
+        password: hashedPassword,
+      };
+
+      if (email) {
+        userProperties.email = email;
+      }
+
+      if (phone) {
+        userProperties.phone = phone;
+      }
+
+      const createUser = await prisma.user.create({
+        data: userProperties,
       });
 
-      res.json({
-        message: "Login successful",
-        token: token,
+      if (!createUser) {
+        return res.status(400).json({
+          message: "Failed to create user",
+        });
+      }
+
+      return res.status(201).json({
+        message: "User created successfully",
+        user: {
+          id: createUser.id,
+          fullName: createUser.fullName,
+          email: createUser.email,
+          phone: createUser.phone,
+        },
       });
     } catch (err) {
       console.error(err);
