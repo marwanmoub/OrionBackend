63f1acdafb325f9e3f0a85d33797f2f8fdeb9d7b
marwan <moubayedmarwancoding@gmail.com>
2026-04-13 19:42:58 +0300

    fix merge issue and add prisma connection

--- controllers/auth.controllers.js
+++ controllers/auth.controllers.js
@@ -35,6 +35,35 @@ const authController = {
       res.status(500).send("Internal Server Error");
     }
   },
+
+  signUp: async (req, res) => {
+    try {
+      let { email, password } = req.body;
+
+      let userFromDB = prisma.users.findUnique({
+        where: {
+          email: email,
+        },
+      }); // TODO: Fix table name
+      if (userFromDB) {
+        return res.status(400).send("Email already exists");
+      }
+      const hashedPassword = await argon2.hash(password);
+      const isValid = await argon2.verify(userFromDB.password, password);
+      if (!isValid) return res.status(401).json({ error: "Wrong Password" });
+      const token = jwt.sign({ userId: user.id }, "Allah_y5lilna_ye@bau$$", {
+        expiresIn: "1h",
+      });
+
+      res.json({
+        message: "Login successful",
+        token: token,
+      });
+    } catch (err) {
+      console.error(err);
+      res.status(500).send("Internal Server Error");
+    }
+  },
 };
 
 export default authController;
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -3,5 +3,6 @@ import authController from "../controllers/auth.controllers";
 const authRouter = express.Router();
 
 authRouter.post("/login", authController.login);
+authRouter.post("/signUp", authController.signUp);
 
 export default authRouter;
