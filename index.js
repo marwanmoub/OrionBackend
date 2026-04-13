e21733276fb62e2ed1cc8d3b5c1c798e666261cc
marwan <moubayedmarwancoding@gmail.com>
2026-04-13 18:59:07 +0300

    Change file structure and fix app routes

--- index.js
+++ index.js
@@ -1,49 +1,19 @@
 import express from "express";
-const { PrismaClient } = require('@prisma/client');
-const argon2 = require('argon2');
-const jwt = require('jsonwebtoken');
+import { argon2 } from "argon2";
+import jwt from "jwt";
+import { PrismaClient } from "@prisma/client/extension";
+import authRouter from "./routes/auth.routes";
+
 const prisma = new PrismaClient();
 const app = express();
 const PORT = 3000;
 
+const router = express.router();
+
 app.use(express.json());
 
-app.post("/Login",async (req, res) => {
-  try{
-  let {email,phone, password} = req.body;
-  
-  let userFromDB = prisma.users.findUnique({
-    where: {
-      email: email, 
-    }
-  });  // TODO: Fix table name]
-  if(!userFromDB){
-   userFromDB = prisma.users.findUnique({
-      where: {
-        phone: phone, 
-      }
-    });  
-  }
-  if(!userFromDB){
-    return res.status(404).send("User not found");
-  }
-  const isValid = await argon2.verify(userFromDB.password, password);
-    if (!isValid) return res.status(401).json({ error: "Wrong Password" });
-    const token = jwt.sign(
-      { userId: user.id }, 
-      'Allah_y5lilna_ye@bau$$', 
-      { expiresIn: '1h' }     
-  );
-
-  res.json({
-      message: "Login successful",
-      token: token
-  });
-}catch(err){
-  console.error(err);
-  res.status(500).send("Internal Server Error");
-}
-});
+//ROUTES
+app.use("/auth", authRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
