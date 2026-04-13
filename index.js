614b8e2884a274a5ad900c989844d9ce5d7c6825
Blockwatch1 <005omarshehab@gmail.com>
2026-04-13 19:06:02 +0300

    .

--- index.js
+++ index.js
@@ -44,6 +44,38 @@ app.post("/Login",async (req, res) => {
   res.status(500).send("Internal Server Error");
 }
 });
+app.post("/SignUpEmail",async (req, res) => {
+  try{
+  let {email, password} = req.body;
+  
+  let userFromDB = prisma.users.findUnique({
+    where: {
+      email: email, 
+    }
+  });  // TODO: Fix table name
+  if(userFromDB){
+    return res.status(400).send("Email already exists"); 
+  }
+  const hashedPassword = await argon2.hash(password);
+  const isValid = await argon2.verify(userFromDB.password, password);
+    if (!isValid) return res.status(401).json({ error: "Wrong Password" });
+    const token = jwt.sign(
+      { userId: user.id }, 
+      'Allah_y5lilna_ye@bau$$', 
+      { expiresIn: '1h' }     
+  );
+
+  res.json({
+      message: "Login successful",
+      token: token
+  });
+}catch(err){
+  console.error(err);
+  res.status(500).send("Internal Server Error");
+}
+});
+
+
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
