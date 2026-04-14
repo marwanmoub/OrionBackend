39427e95cbb155147a801592b115c0ccf7991479
Blockwatch1 <005omarshehab@gmail.com>
2026-04-14 16:20:57 +0300

    shimmy shimmy

--- utils/otp.js
+++ utils/otp.js
@@ -1,15 +1,27 @@
 import NodemailerHelper from 'nodemailer-otp';
-require('dotenv').config();
+import dotenv from 'dotenv';
+import prisma from "/lib/prisma.ts";
+import jwt from "jsonwebtoken";
+dotenv.config();
 
-// Initialize the helper
 const helper = new NodemailerHelper(process.env.EMAIL_USER, process.env.EMAIL_PASS);
 const OTP = {
-    generateOTP: function (length = 6) {
-        let otp = '';
-        for (let i = 0; i < length; i++) {
-            otp += Math.floor(Math.random() * 10); // Generate a random digit (0-9)
-        }
-        return otp;
+    generateOTP: async (req, res) =>{
+        const {token}= req.body;
+        const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+        const userId = decoded.id;
+        const otp = helper.generateOtp(6);
+        try {
+            const response = await helper.sendEmail(
+              'recipient-email@example.com',
+              'subject',
+              'your message here!',
+              otp
+            );
+            console.log(response);
+          } catch (err) {
+            console.error(err);
+          }
     }
 
 }
