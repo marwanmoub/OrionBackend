b79e591e6938b765892428b2b3fcfb98fdbbbe25
marwan <moubayedmarwancoding@gmail.com>
2026-04-14 20:42:15 +0300

    logout

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -3,11 +3,11 @@ import authController from "../controllers/auth.controllers.js";
 import { generateOTP } from "../utils/otp.js";
 import verifyToken from "../middlewares/auth.js";
 const authRouter = express.Router();
+
 authRouter.post("/login", authController.login);
 authRouter.post("/signUp", authController.signUp);
 authRouter.get("/logout", verifyToken, authController.logout);
 
-authRouter.get("/logout", verif);
 authRouter.post("/pass-reset", authController.forgotPassword);
 //OTP Related
 authRouter.post("/verify-email", authController.OTP.checkOTP);
