22939282e2607e4ac284e9ab3f31606e5b7fa951
amen6 <jamen.jamen30@gmail.com>
2026-04-16 10:09:52 +0300

    added change Name and change Password endpoints

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,8 +1,15 @@
 import express from "express";
 import userController from "../controllers/users.controllers";
 import verifyToken from "../middlewares/auth";
+
 const userRouter = express.Router();
 
 userRouter.get("/", verifyToken, userController.getAllUsers);
+userRouter.patch("/change-name", verifyToken, userController.changeName);
+userRouter.patch(
+  "/change-password",
+  verifyToken,
+  userController.changePassword,
+);
 
 export default userRouter;
